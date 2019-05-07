f53e12151a4d84cbdeb5912dc4d699eb8a2c99b8
slabouchei <slabouchei@IF501-202-01.insa-lyon.fr>
2019-05-07 14:43:11 +0200

    Mise à jour

--- src/main/java/fr/insalyon/dasi/projetdasipart2/ActionServlet.java
+++ src/main/java/fr/insalyon/dasi/projetdasipart2/ActionServlet.java
@@ -34,6 +34,7 @@ public class ActionServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
         response.setContentType("text/html;charset=UTF-8");
         String todo = request.getParameter("todo");
         Action action = null;
