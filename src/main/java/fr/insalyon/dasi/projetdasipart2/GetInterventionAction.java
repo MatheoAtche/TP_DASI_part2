425a9120807731d8444fca253fb4bd8d969991d9
Hermora <matheo.atche@gmail.com>
2019-05-21 12:39:32 +0200

    changement cle api et correction erreur

--- src/main/java/fr/insalyon/dasi/projetdasipart2/GetInterventionAction.java
+++ src/main/java/fr/insalyon/dasi/projetdasipart2/GetInterventionAction.java
@@ -20,15 +20,26 @@ public class GetInterventionAction extends Action {
     public boolean executer(HttpServletRequest request) {
         Service service = new Service();
         Employe employe = (Employe)request.getSession().getAttribute("employe");
-        Intervention i = service.getInterventionEnCours(employe);
-        request.setAttribute("client", i.getClient().getNom() + " " + i.getClient().getPrenom());
-        request.setAttribute("tel", i.getClient().getTelephone());
-        request.setAttribute("type", i.typeToString());
-        request.setAttribute("message", i.getDescription());
-        request.setAttribute("heure", i.getDebut());
-        request.setAttribute("adresse", i.getClient().getAdresse().toString());
-        request.setAttribute("lat",i.getClient().getAdresse().getLatitude());
-        request.setAttribute("long", i.getClient().getAdresse().getLongitude());
+        if (!employe.isDisponible()){
+            Intervention i = service.getInterventionEnCours(employe);
+            request.setAttribute("client", i.getClient().getNom() + " " + i.getClient().getPrenom());
+            request.setAttribute("tel", i.getClient().getTelephone());
+            request.setAttribute("type", i.typeToString());
+            request.setAttribute("message", i.getDescription());
+            request.setAttribute("heure", i.getDebut());
+            request.setAttribute("adresse", i.getClient().getAdresse().toString());
+            request.setAttribute("lat",i.getClient().getAdresse().getLatitude());
+            request.setAttribute("long", i.getClient().getAdresse().getLongitude());
+        }else{
+            request.setAttribute("client", null);
+            request.setAttribute("tel", null);
+            request.setAttribute("type", null);
+            request.setAttribute("message", null);
+            request.setAttribute("heure", null);
+            request.setAttribute("adresse", null);
+            request.setAttribute("lat",null);
+            request.setAttribute("long", null);
+        }
         return true;
     }
 }
